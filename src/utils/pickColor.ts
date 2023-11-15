81d53da9d54992baf5b7a340f7cff497712f7424
zahid47 <epiczahid@gmail.com>
2023-11-15 15:54:23 +0600

    Refactor pickColor function to accept options

--- src/utils/pickColor.ts
+++ src/utils/pickColor.ts
@@ -1,10 +1,32 @@
-const github_light = ["#9be9a8", "#40c463", "#30a14e", "#216e39"] as const;
-const GRAY = "#ebedf0";
+type Options = {
+  mode: "light" | "dark";
+  color: keyof typeof COLORS;
+};
 
-export const pickColor = (contributions: number[], today: number) => {
-  const colorScheme = github_light;
+const GRAY = {
+  light: "#ebedf0",
+  dark: "#ebedf0",
+} as const;
 
-  if (today === 0) return GRAY;
+const COLORS = {
+  github_green: {
+    light: ["#9be9a8", "#40c463", "#30a14e", "#216e39"],
+    dark: ["#9be9a8", "#40c463", "#30a14e", "#216e39"],
+  },
+} as const;
+
+
+export const pickColor = (
+  contributions: number[],
+  today: number,
+  options: Options = {
+    mode: "light",
+    color: "github_green",
+  }
+) => {
+  const colorScheme = COLORS[options.color][options.mode];
+
+  if (today === 0) return GRAY[options.mode];
 
   const max = Math.max(...contributions);
   const normalizedToday = Math.round((today / max) * 3);
